d00d4532d36834bf24baedf35ec066055545476b
pp <funp992021@163.com>
2021-12-18 12:33:20 +0800

    结构样式

--- data/src/views/Home.vue
+++ data/src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="content">
+       <div class="content-header"></div>
+       <div class="content-center">
+               <section class="contenCenterLeft">
+                  <div class="left-data">
+                     <div class="left-data-center">
+                          <ul>
+                             <li>1</li>
+                             <li>2</li>
+                             <li>3</li>
+                             <li>4</li>
+                          </ul>
+                     </div>
+                  </div>
+               </section>
+               <section class="contentCenterCen">2</section>
+               <section class="contentCenterright">3</section>
+       </div>
+    </div> 
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+   data() {
+      return {
+      }
+   },
+   created(){
+   },
+   computed:{
+   },
+   methods:{
+   },
 }
 </script>
+<style scoped>
+     .content-header{
+        width: 100%;
+        height: 80px; 
+        background: url('~@/assets/images/logo.png');
+        background-size: 100% 100%;
+     }
+     .content .content-center{
+        display: flex;
+        color: blue;
+     }
+     .content .content-center section{
+        flex: 3;
+     }
+     .content-center section:nth-child(2){
+        flex: 4;
+     }
+     .content-center section:nth-child(3){
+        flex: 3;
+     }
+     .content-center .contenCenterLeft .left-data{
+        position: relative;
+        border: 15px solid transparent;
+        border-width: 51px 38px 20px 132px;
+        border-image-source: url('~@/assets/images/border.png');
+        border-image-slice: 51 38 20 132;
+        margin-left: 52px;
+     }
+     .content-center .contenCenterLeft .left-data .left-data-center{
+        position: absolute;
+        top: -50px;
+        bottom: -20px;
+        left:-132px;
+        right:-38px;
+     }
+     .content-center .contenCenterLeft .left-data .left-data-center ul{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        list-style: none;
+     }
+     .content-center .contenCenterLeft .left-data .left-data-center ul li{
+        flex: 1;
+     }
+</style>
